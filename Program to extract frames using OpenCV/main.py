b2e928ddbd7aeb46c81447264d0957fe96b83f7a
IT20600334 - Eranda Withanage <88669615+it20600334@users.noreply.github.com>
2023-10-04 00:08:25 +0530

    Completing

--- Program to extract frames using OpenCV/main.py
+++ Program to extract frames using OpenCV/main.py
@@ -1,1 +1,25 @@
 #Python | Program to extract frames using OpenCV
+
+import cv2
+
+def FrameCapture(path):
+
+	vidObj = cv2.VideoCapture(path)
+
+	count = 0
+
+	success = 1
+
+	while success:
+
+		success, image = vidObj.read()
+
+		cv2.imwrite("capture%d.jpg" % count, image)
+
+		count += 1
+
+
+
+if __name__ == '__main__':
+
+	FrameCapture("C:\\Users\\ERANDA WITHANAGE\\Downloads\\pexels_videos_1249406 (1080p).mp4")
